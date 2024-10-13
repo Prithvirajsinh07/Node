c654a129fdc76c7a70989ba7dc3fb91a024f1d6b
Prithvirajsinh07 <pgdodiya7208@gmail.com>
2024-10-13 21:48:47 +0530

    commit messege

---
+++ Project/if else/index.js
@@ -0,0 +1,32 @@
+const http = require("http");
+const fs = require("fs");
+
+const myServer = http.createServer((req, res) => {
+  //   console.log("New Request Recived");
+  // console.log(req);
+  // console.log(req.headers);
+
+  const log = New Request Recived : ${Date.now()} : ${req.url} \n;
+
+  fs.appendFile("log.txt", log, (err, data) => {
+    //  res.end("Namaste User");
+
+    switch (req.url) {
+      case "/":
+        res.end("Namaste Home Page");
+
+        break;
+      case "/about":
+        res.end("Namaste About Page");
+
+        break;
+
+      default:
+        res.end("404 Error, Page Not Found!");
+    }
+  });
+});
+
+myServer.listen(8000, () => {
+  console.log("Server Started");
+});
